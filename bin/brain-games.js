add833b0494de11e8c100b0f8eccf4fb6002d95d
Maxim Klechikov <maximkakl1@gmail.com>
2022-12-04 19:07:20 +0300

    update bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import readName from '../src/cli';
 
 console.log('Welcome to the Brain Games!');
+console.log(`May I have your name? ${readName}`);
+console.log(`Hello, ${readName}`);
