87c336f4457a83a7f7d759c293f87cde6ef2f97c
Maxim Klechikov <maximkakl1@gmail.com>
2022-12-04 19:28:11 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
-// import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-const readlineSync = require('readline-sync');
-
-const readName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
+const name = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
-export default readName;
+export default name;
