fcb30ee406a75c7756939c1bc9ae941fdaba97da
Maxim Klechikov <maximkakl1@gmail.com>
2022-12-07 19:44:11 +0300

    update bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,26 +1,4 @@
-import readlineSync from 'readline-sync';
-import name from '../src/cli.js';
+#!/usr/bin/env node
+import brainEven from '../src/be.js';
 
-const brain_even = () => {
-  const userName = name();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.floor(Math.random() * 100);
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if ((number % 2 === 0 && userAnswer === 'yes') || (number % 2 !== 0 && userAnswer === 'no')) {
-      console.log('Correct!');
-    } else if (number % 2 === 0 && userAnswer === 'no'){
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    } else if (number % 2 !== 0 && userAnswer === 'yes') {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-brain_even();
+brainEven();
