d2b4e1a52b3118998c47e013dbd660ef2ad7aa24
Maxim Klechikov <maximkakl1@gmail.com>
2022-12-15 20:56:49 +0300

    update src/games/brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,6 +1,27 @@
 import randomNumber from '../randomNumber.js';
 import startGame from '../index.js'
 
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const searchGcd = (firstNumber, secondNumber) => {
+  while (firstNumber != 0 && secondNumber != 0) {
+    if (firstNumber > secondNumber) {
+      firstNumber = firstNumber % secondNumber;
+    } else {
+      secondNumber = secondNumber % firstNumber;
+    }
+  }
+  return String(firstNumber + secondNumber);
+};
+
+const getQuestionAndAnswer = () => {
+  const firstNumber = randomNumber(0, 10);
+  const secondNumber = randomNumber(0, 10);
+  const question = `${firstNumber} ${secondNumber}`;
+  const answer = searchGcd(firstNumber, secondNumber);
+  return [question, answer];
+};
+
 const brainGcd = () => {
   startGame(gameDescription, getQuestionAndAnswer);
 }
