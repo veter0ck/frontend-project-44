9c21cb967a3f5c28708cbbc9e316a9aaaaa6f505
Maxim Klechikov <maximkakl1@gmail.com>
2022-12-15 19:33:58 +0300

    update src/games/brain-gcd.js

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,8 @@
+import randomNumber from '../randomNumber.js';
+import startGame from '../index.js'
+
+const brainGcd = () => {
+  startGame(gameDescription, getQuestionAndAnswer);
+}
+
+export default brainGcd;
