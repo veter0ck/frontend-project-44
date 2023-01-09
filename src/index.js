83b5abc3cffca3b354819be7dc1fad9cb82ecf49
Maxim Klechikov <maximkakl1@gmail.com>
2023-01-09 15:17:46 +0300

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
-import runGreeting from './cli.js';
 
 const startGame = (gameDescription, generateNewRound) => {
-  const userName = runGreeting();
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(gameDescription);
   const countOfRound = 3;
 
