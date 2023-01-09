75bfac58c573bfa67865b5751ac7b58017053a47
Maxim Klechikov <maximkakl1@gmail.com>
2023-01-09 16:48:38 +0300

    updated src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,5 +1,5 @@
 import getRandomNumber from '../randomNumber.js';
-import startGame from '../index.js'
+import startGame from '../index.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -10,12 +10,12 @@ const isEven = (num) => num % 2 === 0;
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(minRange, maxRange);
-  const answer = isEven(question)? 'yes' : 'no';
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
 const runBrainEven = () => {
   startGame(gameDescription, getQuestionAndAnswer);
-}
+};
 
 export default runBrainEven;
